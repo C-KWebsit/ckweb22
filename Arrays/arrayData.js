51e1f6b1a78364fadc5df6cb8405f817320065b0
Kyle McDonald <kylemcdonald@Kyles-MacBook-Pro.local>
2021-02-02 23:27:43 -0700

    added the array data structures

---
+++ Arrays/arrayData.js
@@ -0,0 +1,38 @@
+
+export const arr1 = [
+    { 'first': 'This is the first element of the array' },
+    { 'second': 'This is the second element of the array' },
+    { 'third': 'This is the third element of the array' },
+    { 'fourth': 'This is the fourth element of the array' },
+    { 'fifth': 'This is the fifth element of the array' },
+    { 'sixth': 'This is the first element of the array' } 
+];
+
+export const arr2 = [
+    1,
+    2,
+    'Some string',
+    { 'object': 'Some Object' },
+    3,
+    true,
+    false
+];
+
+
+export const arr3 = [
+    ['This is some array with an array'],
+    ['This is some array within an array at index 1'],
+    ['This is some index within an array and well you get the point'],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    ['It should be obvious now that you can have different data type in an array although it is not good practice'],
+    ['When you create your own data structures it is best practice to use the same data types within that array']
+];
+
+export const arr4 = [
+    [1, 'key1'],
+    [2, 'key2'],
+    [3, 'key3'],
+    [4, 'key4'],
+    [5, 'key5']
+];
+
